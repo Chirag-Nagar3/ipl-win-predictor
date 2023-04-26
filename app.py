85a425c137e07d4cd674cc17ed1e2d90cf0d0ddf
Chirag1048 <128238388+Chirag1048@users.noreply.github.com>
2023-04-26 19:45:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,15 +25,15 @@ data = response.json()
 
 myId = "9513145b-b401-4e72-a898-a4bd7c688be8"
 permission  = "yes"
-match_id = data['data'][4]['id']
-match_name = data['data'][4]['name']
-match_city = data['data'][4]['venue']
-target_score = data['data'][4]['score'][0]['r']
-current_score = data['data'][4]['score'][1]['r']
-fall_wickets = data['data'][4]['score'][1]['w']
-over_completed = data['data'][4]['score'][1]['o']
-batting_team = data['data'][4]['teams'][1]
-bowling_team = data['data'][4]['teams'][0]
+match_id = data['data'][6]['id']
+match_name = data['data'][6]['name']
+match_city = data['data'][6]['venue']
+target_score = data['data'][6]['score'][0]['r']
+current_score = data['data'][6]['score'][1]['r']
+fall_wickets = data['data'][6]['score'][1]['w']
+over_completed = data['data'][6]['score'][1]['o']
+batting_team = data['data'][6]['teams'][1]
+bowling_team = data['data'][6]['teams'][0]
 
 
 pipe = pickle.load(open('pipe11.pkl','rb'))
